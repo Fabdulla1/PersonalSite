e0abf674674c7b15ea85e339a958ba924d251cba
Farhan <farhan_abdulla@berkeley.edu>
2025-06-18 23:32:26 -0700

    Added header and buttons as well as routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const buttonNames = ['Me', 'About', 'Contact'];
+const routes = ['/me', '/about', '/contact'];
+
+function buttons() {
+   return (
+    <div style={{ display: 'flex', gap: '10px' }}>
+      {buttonNames.map((label, index) => (
+        <Link to={routes[index]} key={index}>
+          <button
+            style={{
+              backgroundColor: "blueviolet"
+            }}
+          >{label}</button>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        className="header"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between', 
+          padding: '10px',
+        }}
+      >
+        <div>Farhan Abdulla</div>
+        {buttons()}
       </header>
     </div>
   );
